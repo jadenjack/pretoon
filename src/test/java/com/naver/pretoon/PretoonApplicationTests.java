b845829bf022dd04550be5a810a30fe0262b0690
jadenjack <jij5379@naver.com>
2018-11-03 22:03:48 +0900

    add)test ContextConfiguration

--- src/test/java/com/naver/pretoon/PretoonApplicationTests.java
+++ src/test/java/com/naver/pretoon/PretoonApplicationTests.java
@@ -3,10 +3,13 @@ package com.naver.pretoon;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes=PretoonApplicationTests.class, loader=AnnotationConfigContextLoader.class)
 public class PretoonApplicationTests {
 
 	@Test
