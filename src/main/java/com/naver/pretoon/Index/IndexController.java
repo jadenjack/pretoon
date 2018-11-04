2592e942864c78b59ec795b9c8ff0bf0757d2a98
jadenjack <jij5379@naver.com>
2018-11-04 23:25:01 +0900

    change pw

--- src/main/java/com/naver/pretoon/Index/IndexController.java
+++ src/main/java/com/naver/pretoon/Index/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/pretoon?useSSL=false";
 	private static final String USER="root";
-	private static final String PW = "jadenjack2@R";
+	private static final String PW = "dydwns2";
 	
 	@RequestMapping("/")
 	public String index(Model model) throws Exception{
